f60502c81b1920f1c399e95f8b566513b08ffd04
Victor <victorroldan.dev@gmail.com>
2022-02-20 12:22:46 -0500

    clase nro 2

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Victor Roldan on 20/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
