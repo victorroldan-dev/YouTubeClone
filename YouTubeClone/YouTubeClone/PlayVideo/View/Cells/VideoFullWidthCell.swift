c0c8859abf62f975250ffe8a609352f5f286bb1f
Victor <victorroldan.dev@gmail.com>
2022-03-30 19:14:26 -0500

    VideoFullWidthCell

--- YouTubeClone/YouTubeClone/PlayVideo/View/Cells/VideoFullWidthCell.swift
+++ YouTubeClone/YouTubeClone/PlayVideo/View/Cells/VideoFullWidthCell.swift
@@ -9,12 +9,29 @@ import UIKit
 
 class VideoFullWidthCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    func configCell(model : VideoModel.Item){
+    @IBOutlet weak var videoImage: UIImageView!
+        @IBOutlet weak var videoTitle: UILabel!
+        @IBOutlet weak var statistics: UILabel!
         
-    }
+        override func awakeFromNib() {
+            super.awakeFromNib()
+        }
+
+        func configCell(model : VideoModel.Item){
+            videoTitle.text = model.snippet?.title
+            videoTitle.textColor = .whiteColor
+            
+            let channelTitle = model.snippet?.channelTitle ?? ""
+            //let pusblished = model.snippet?.publishedDateFriendly ?? ""
+            let randInt = Int.random(in: 200..<999)
+            statistics.text = "\(channelTitle) · \(randInt) views · 3 days ago"
+            statistics.textColor = .grayColor
+            
+            guard let imageUrl = model.snippet?.thumbnails.medium?.url, let url = URL(string: imageUrl) else{
+                videoImage.image = .videoPlaceholder
+                return
+            }
+            videoImage.kf.setImage(with: url, placeholder: UIImage.videoPlaceholder)
+        }
+        
 }
